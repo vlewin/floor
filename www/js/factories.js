26eaa55251d4395a9290835e5f4f6551fe2f6f2e
Vladislav Lewin <vlewin@suse.de>
2015-04-14 14:26:00 +0200

    update resource URL

--- www/js/factories.js
+++ www/js/factories.js
@@ -1,3 +1,3 @@
 angular.module('floor.services', []).factory('Employee', function($resource) {
-  return $resource('http://localhost:3001/employees/:id');
+  return $resource('http://stealth-new.suse.de:3001/employees/:id');
 });
