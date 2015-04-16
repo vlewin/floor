40769ee0b89da36ef998532631b0202b6f1cabcb
Thomas Schmidt <tschmidt@suse.de>
2015-04-16 16:13:59 +0200

    change to employee id

--- www/js/factories.js
+++ www/js/factories.js
@@ -1,3 +1,3 @@
-angular.module('floor.services', []).factory('Employee', function($resource) {
-  return $resource('http://stealth-new.suse.de:3001/employees/:id');
+angular.module('floor.services', []).factory('Employee', function($rootScope, $resource) {
+  return $resource($rootScope.server + '/employees/:id');
 });
